d18874c9b262e052138ee18e4671d0a5814a2884
HaAlpaca <dangminhha5@gmail.com>
2023-07-19 16:04:11 +0700

    Dựng Header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,33 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
-const cx = classNames.bind(styles)
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles);
 
 function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        {/* logo */}</div>
+        {/* logo */}
+        <img src={images.logo} alt="TikTok"></img>
+
+        <div className={cx('search')}>
+          <input placeholder="Search accounts & videos" spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={cx('search-btn')}>
+            {/* search */}
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+
+        <div className={cx('action')}></div>
+      </div>
     </header>
   );
 }
